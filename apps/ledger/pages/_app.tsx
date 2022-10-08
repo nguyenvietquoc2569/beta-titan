c2735ae5fbc53dc37a4b5e95bdcf5bf5e861fecd
Harry Nguyen <Harry.Nguyen@ocean.ibm.com>
2022-10-07 21:58:22 -0500

    add pages

--- apps/ledger/pages/_app.tsx
+++ apps/ledger/pages/_app.tsx
@@ -1,18 +1,100 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+// import for utils/highlight.ts
+import '@beta-titan/ledger/frontend/utilities/ui-settings-fe'
+// i18n
+// import '../locales/i18n';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+// lightbox
+import 'react-image-lightbox/style.css';
+
+// map
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+// editor
+import 'react-quill/dist/quill.snow.css';
+
+// slick-carousel
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+// lazy image
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
+
+// fullcalendar
+import '@fullcalendar/common/main.min.css';
+import '@fullcalendar/daygrid/main.min.css';
+
+import cookie from 'cookie';
+import { ReactElement, ReactNode } from 'react';
+// next
+import { NextPage } from 'next';
+import Head from 'next/head';
+import App, { AppProps, AppContext } from 'next/app';
+//
+import { Provider as ReduxProvider } from 'react-redux';
+// @mui
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { CollapseDrawerProvider, getSettings, MotionLazyContainer, NotistackProvider, SettingsProvider, SettingsValueProps, ThemeProvider, ThemeSettings } from '@beta-titan/ledger/frontend/utilities/ui-settings-fe';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+interface MyAppProps extends AppProps {
+  settings: SettingsValueProps;
+  Component: NextPageWithLayout;
+}
+
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, pageProps, settings } = props;
+
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <>
       <Head>
-        <title>Welcome to ledger!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <CollapseDrawerProvider>
+              <SettingsProvider defaultSettings={settings}>
+                <MotionLazyContainer>
+                  <ThemeProvider>
+                    <ThemeSettings>
+                      <NotistackProvider>
+                        {/* <ChartStyle /> */}
+                        {/* <ProgressBar /> */}
+                        {getLayout(<Component {...pageProps} />)}
+                      </NotistackProvider>
+                    </ThemeSettings>
+                  </ThemeProvider>
+                </MotionLazyContainer>
+              </SettingsProvider>
+            </CollapseDrawerProvider>
+          </LocalizationProvider>
     </>
   );
 }
 
-export default CustomApp;
+MyApp.getInitialProps = async (context: AppContext) => {
+  const appProps = await App.getInitialProps(context);
+
+  const cookies = cookie.parse(
+    context.ctx.req ? context.ctx.req.headers.cookie || '' : document.cookie
+  );
+
+  const settings = getSettings(cookies);
+
+  return {
+    ...appProps,
+    settings,
+  };
+};
+
